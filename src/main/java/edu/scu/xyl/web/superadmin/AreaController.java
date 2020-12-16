0a4a0565565be2822ffdd48220bcd393e2c94b3b
Dylan <liuxiangyu0613@gmail.com>
2020-12-15 17:56:41 -0800

    add log part

--- src/main/java/edu/scu/xyl/web/superadmin/AreaController.java
+++ src/main/java/edu/scu/xyl/web/superadmin/AreaController.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +19,17 @@ import edu.scu.xyl.service.AreaService;
 @Controller
 @RequestMapping("/superadmin")
 public class AreaController {
+	Logger logger = LoggerFactory.getLogger(AreaController.class);
+	
+	
 	@Autowired
 	private AreaService areaService;
 
 	@RequestMapping(value = "/listArea", method = RequestMethod.GET)
 	@ResponseBody
 	private Map<String, Object> listArea() {
+		logger.info("===start===");
+		long startTime = System.currentTimeMillis();
 		Map<String, Object> modelMap = new HashMap<>();
 		List<Area> list = new ArrayList();
 		try {
@@ -35,6 +42,10 @@ public class AreaController {
 			modelMap.put("errMsg", e.toString());
 		}
 
+		logger.error("test error!");
+		long endTime = System.currentTimeMillis();
+		logger.debug("costTime:[{}ms]", endTime-startTime);
+		logger.info("===end===");
 		return modelMap;
 	}
 
